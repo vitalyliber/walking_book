afe7c56f84d89f17910c57a6c3847cf9d20291de
irenliber <irr.morgo@gmail.com>
2017-09-02 17:00:53 +0700

    added enums for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
 
+  enum status: [:available, :busy]
+  enum category: [:fiction, :education, :children, :other]
+
   belongs_to :author
   has_many   :histories
   has_many   :users, through: :histories
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902091410) do
+ActiveRecord::Schema.define(version: 20170902093456) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170902091410) do
     t.string "title"
     t.text "description"
     t.string "category"
-    t.string "status"
+    t.string "status", default: "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
