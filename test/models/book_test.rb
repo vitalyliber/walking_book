2b1d929afcf7f3c919bc77b0e812c93da4587ad9
irenliber <irr.morgo@gmail.com>
2017-09-02 18:20:42 +0700

    edited book test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
 
+  test "should save book" do
+    book = Book.new
+    author = create(:author)
+    book.title = 'New book'
+    book.author = author
+    assert book.save
+  end
+
   test "should not save book without title" do
     book = Book.new
     author = create(:author)
