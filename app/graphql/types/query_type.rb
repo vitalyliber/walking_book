6dc19fbec4d0f29868f758bfc89f6693383546d7
Vitaly Liber <liber@sibext.com>
2017-09-02 17:16:16 +0700

    added a book type to a graphQL

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,5 @@
+Struct.new("Book", :title, :description, :id, :author_id)
+
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
   # Add root-level fields here.
@@ -8,6 +10,20 @@ Types::QueryType = GraphQL::ObjectType.define do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
       "Hello World!"
+    }
+  end
+
+  field :books, types[Types::BookType] do
+    description "Book field"
+    resolve ->(obj, args, ctx) {
+      [
+        Struct::Book.new(
+          'Harry Potter and prisoner of Askaban',
+          'Once was a boy named Harry',
+          1,
+          2
+        )
+      ]
     }
   end
 end
