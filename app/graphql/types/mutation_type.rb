eba064834ee3a96081a96d729752d7ec081fd9d7
Vitaly Liber <liber@sibext.com>
2017-09-03 23:18:56 +0700

    created a mutation for a creating new feedback

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
+  field :createFeedback, Mutations::CreateFeedback.field
   field :createAuthor, Mutations::CreateAuthor.field
   field :createBook, Mutations::CreateBook.field
 end
